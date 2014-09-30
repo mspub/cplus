37138eb192e9c22a90939830627bac4cb70ff6c7
na <munsam.it@gmail.com>
2014-10-01 04:17:17 +0900

    c variable

---
+++ var.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int data=10;
+void func()
+{
+    cout<<"전역 함수 func 호출\n";
+}
+ 
+namespace X
+{
+    int data=20;
+    void func()
+    {
+    cout<<"X::func 호출\n";
+    }
+}
+     
+int main()
+{
+    using namespace X;
+    int data=30;
+    cout<<"data="<<data<<"\n"; //지역변수
+    cout<<"data="<<X::data<<"\n";//네임스페이스내 전역변수
+    cout<<"data="<<::data<<"\n";  //전역변수
+     
+   X::func();
+   ::func();
+   return 0;
+}
