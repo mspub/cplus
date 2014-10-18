5894410aaff9d43e330472809416e3e9617e57fb
na <munsam.it@gmail.com>
2014-10-18 23:28:26 +0900

    함수 원형과 정의를 메일 앞 뒤에 작성

--- vector_countword.cpp
+++ vector_countword.cpp
@@ -4,7 +4,23 @@
 
 using namespace std;
 
+struct WORD
+{
+	string str;
+	int count;
+};
+
+vector<WORD*> words;
+int FindWords(const string & s);
+int CountWords(const string & s);
+void ShowWords();
+void RemoveAll();
+
 int main()
 {
 	return 0;
 }
+int FindWords(const string & s){return 0;}
+int CountWords(const string & s){return 0;}
+void ShowWords(){}
+void RemoveAll(){}
