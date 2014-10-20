f09f5971f678180704dcc2d4bd550ab69f2168db
na <munsam.it@gmail.com>
2014-10-20 22:40:49 +0900

    주석달기

--- cpp9-14.cpp
+++ cpp9-14.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class Complex
+class Complex //복소수 
 {
 private :
 	int real;
 	int image;
 public:
-	Complex(int r=0,int i=0);
+	Complex(int r=0,int i=0);//생성자
 	void showComplex() const;
 };
 
@@ -15,18 +15,18 @@ Complex::Complex(int r, int i):real(r),image(i){}
 
 void Complex::showComplex() const
 {
-	cout<<"(" <<real<<"+"<<image<<"i)"<<endl;
+	cout<<"(" <<real<<" + "<<image<<"i)"<<endl;
 }
 
 void prn(Complex *pCom)
 {
 	for(int i=0;i<4;i++)
-		pCom[i].showComplex();
-	pCom->showComplex();
-	(pCom+1)->showComplex();
+		pCom[i].showComplex(); //배열로 표시
+	pCom->showComplex();       //pCom[0].showComplex()
+	(pCom+1)->showComplex();   //pCom[i].showComplex()
 }
 
-void main()
+void main() //메인함수
 {
 	Complex arr[4]={ Complex(2,4),Complex(4,8),Complex(8,16),Complex(16,32)};
 	prn(arr);
